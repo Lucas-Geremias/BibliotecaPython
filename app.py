c497a7e793b6e1446f5deff922aa129fa15b0124
Lucas Geremias <44319767+Lucas-Geremias@users.noreply.github.com>
2025-10-28 10:29:35 -0300

    menu

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# ...existing code...
 import mysql.connector
 
 
@@ -18,7 +19,7 @@ def adicionar_livro(titulo, autor, ano_publicacao):
   sql = "INSERT INTO livros(titulo, autor, ano_publicacao) VALUES (%s, %s, %s)"
   
   valores = (titulo, autor, ano_publicacao)
-  cursor.execute(sql,valores)
+  cursor.execute(sql, valores)
   conexao.commit()
   conexao.close()
   print("Livro Adicionado com sucesso!!!!")
@@ -28,7 +29,7 @@ def listar_livros():
   cursor = conexao.cursor()
   
   cursor.execute("SELECT * FROM livros")
-  livros = cursor.fetchall #pega todos os resultados 
+  livros = cursor.fetchall()  # pega todos os resultados 
   conexao.close()
   
   if livros:
@@ -38,16 +39,15 @@ def listar_livros():
   else:
     print("Nenhum livro encontrado.")
 
-#Atualizar os livros
-
-def atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano ):
+# Atualizar os livros
+def atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano):
   conexao = conectar()
   cursor = conexao.cursor()
   
   sql = """
     UPDATE livros
-    SET titulo = %s, autor = %s, ano_pubicacao = %s
-    WHERE id %s
+    SET titulo = %s, autor = %s, ano_publicacao = %s
+    WHERE id = %s
   """
   valores = (novo_titulo, novo_autor, novo_ano, id_livro)
   
@@ -56,36 +56,62 @@ def atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano ):
   conexao.close()
   print("Livro atualizado com sucesso!!!")
   
-  ##DELETE - EXCLUIR UM LIVRO
-  
-  def excluir_livro(id_livro):
-    conexao = conectar()
-    cursor = conexao.cursor()
+# DELETE - EXCLUIR UM LIVRO
+def excluir_livro(id_livro):
+  conexao = conectar()
+  cursor = conexao.cursor()
 
-    cursor.execute("DELETE FROM livros  WHERE id = %s", (id_livro,))
-    conexao.commit()
-    conexao.close()
+  cursor.execute("DELETE FROM livros WHERE id = %s", (id_livro,))
+  conexao.commit()
+  conexao.close()
     
-    print("Livro excluido com sucesso")
-
+  print("Livro excluido com sucesso")
     
-    ##MENU 
-    def menu():
-      while true:
-        print("=== SISTEMA DE BIBLIOTECA ===")
-        print("1 - Adicionar")
-        print("2 - Listar Livros")
-        print("3 - Atualizar Livros")
-        print("4 - Excluir Livros")
-        print("0 - Sair")
+# MENU 
+def menu():
+  while True:
+    print("=== SISTEMA DE BIBLIOTECA ===")
+    print("1 - Adicionar")
+    print("2 - Listar Livros")
+    print("3 - Atualizar Livros")
+    print("4 - Excluir Livros")
+    print("0 - Sair")
 
-        opcao = input("Escolha uma opção")
+    opcao = input("Escolha uma opção: ")
         
-        #Cadastrar livro
-        if opcao == "1":
-          titulo = input("Título: ")
-          autor = input("Autor: ")
-          ano = input("Ano de publicação: ")
-          adicionar_livro(titulo,autor,ano)
+    # Cadastrar livro
+    if opcao == "1":
+      titulo = input("Título: ")
+      autor = input("Autor: ")
+      ano = input("Ano de publicação: ")
+      adicionar_livro(titulo, autor, ano)
           
-    
+    # Listar livros
+    elif opcao == "2":
+      listar_livros()
+          
+    # Atualizar livros
+    elif opcao == "3":
+      listar_livros()
+      id_livro = input("ID do livro a atualizar: ")
+      novo_titulo = input("Novo título: ")
+      novo_autor = input("Novo Autor: ")
+      novo_ano = input("Novo Ano: ")
+      atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano)
+        
+    # Deletar livros
+    elif opcao == "4":
+      listar_livros()
+      id_livro = input("ID do livro a excluir: ")
+      excluir_livro(id_livro)
+          
+    # Encerrar o sistema
+    elif opcao == "0":
+      print("Saindo do Sistema")
+      break
+    else:
+      print("Tente novamente. ")
+          
+if __name__ == "__main__":
+  menu()
+# ...existing code...
